e66b2bd76245f1b2785cf264b7ac01017d1e520a
ELYAHYAOUY-FL <imane.elyahyaouy@etu.uae.ac.ma>
2024-06-19 19:18:45 +0100

    fix erres

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -2,47 +2,12 @@ import React from "react";
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
-import { services } from "../../constants";
+
 import { SectionWrapper } from "../../hoc";
 import { fadeIn } from "../../utils/motion";
 import { config } from "../../constants/config";
 import { Header } from "../atoms/Header";
 
-
-interface IServiceCard {
-  index: number;
-  title: string;
-  icon: string;
-}
-
-const ServiceCard: React.FC<IServiceCard> = ({ index, title, icon }) => (
-  <Tilt
-    glareEnable
-    tiltEnable
-    tiltMaxAngleX={30}
-    tiltMaxAngleY={30}
-    glareColor="#aaa6c3"
-  >
-    <div className="xs:w-[250px] w-full">
-      <motion.div
-        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-        className="green-pink-gradient shadow-card w-full rounded-[20px] p-[1px]"
-      >
-        <div className="bg-tertiary flex min-h-[280px] flex-col items-center justify-evenly rounded-[20px] px-12 py-5">
-          <img
-            src={icon}
-            alt="web-development"
-            className="h-16 w-16 object-contain"
-          />
-
-          <h3 className="text-center text-[20px] font-bold text-white">
-            {title}
-          </h3>
-        </div>
-      </motion.div>
-    </div>
-  </Tilt>
-);
 
 
 interface IEducationCard {
@@ -91,10 +56,11 @@ const About = () => {
       <Header useMotion={true} {...config.sections.education} />
 
       <div className="mt-20 flex flex-wrap gap-10 max-sm:justify-center">
-        {config.sections.education.items.map((item, index) => (
-          <EducationCard key={index} {...item} />
-        ))}
-      </div>
+  {config.sections.education.items?.map((item, index) => (
+    <EducationCard key={index} {...item} />
+  ))}
+</div>
+
 
      
     </>
--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,6 +2,7 @@ type TSection = {
   p: string;
   h2: string;
   content?: string;
+  items?: IEducationCard[]; // Make items optional
 };
 
 type TConfig = {
@@ -15,6 +16,8 @@ type TConfig = {
     p: string[];
   };
   contact: {
+    p: string;
+    h2: string;
     form: {
       name: {
         span: string;
@@ -29,15 +32,21 @@ type TConfig = {
         placeholder: string;
       };
     };
-  } & TSection;
+  };
   sections: {
-    about: Required<TSection>;
-    experience: TSection;
-    education: TSection;
-    feedbacks: TSection;
-    works: Required<TSection>;
+    about: Required<TSection>; // 'items' is not required here
+    experience: TSection; // 'items' is not required here
+    education: TSection; // 'items' is required here
+    feedbacks: TSection; // 'items' is not required here
+    works: Required<TSection>; // 'items' is required here
   };
 };
+
+interface IEducationCard {
+  date: string;
+  title: string;
+  institution: string;
+}
 
 export const config: TConfig = {
   html: {
@@ -57,7 +66,10 @@ export const config: TConfig = {
         span: "Your Name",
         placeholder: "What's your name?",
       },
-      email: { span: "Your Email", placeholder: "What's your email?" },
+      email: {
+        span: "Your Email",
+        placeholder: "What's your email?",
+      },
       message: {
         span: "Your Message",
         placeholder: "What do you want to say?",
@@ -68,36 +80,34 @@ export const config: TConfig = {
     about: {
       p: "Introduction",
       h2: "Overview.",
-      
+      items: [],
       content: `As a passionate software engineer and Master's student in IT Security and Big Data, I am committed to exploring the latest technologies and solving complex problems. My background reflects a blend of analytical thinking, innovative problem-solving, and effective teamwork, underscoring my dedication and rigorous work ethic.`,
-      
     },
     education: {
       p: "",
       h2: "Education.",
-      items:[
+      items: [
         {
-            "date": "2023 - 2025",
-            "title": "Master's in IT Security and Big Data",
-            "institution": "Faculty of Sciences and Techniques in Tangier"
+          date: "2023 - 2025",
+          title: "Master's in IT Security and Big Data",
+          institution: "Faculty of Sciences and Techniques in Tangier",
         },
         {
-            "date": "2019 - 2023",
-            "title": "Bachelor's  in Mathematical and Computer Sciences",
-            "institution": "Multidisciplinary Faculty of Larache"
+          date: "2019 - 2023",
+          title: "Bachelor's in Mathematical and Computer Sciences",
+          institution: "Multidisciplinary Faculty of Larache",
         },
         {
-            "date": "2022",
-            "title": "CCNA (Cisco Certified Network Associate)",
-            "institution": "Cisco Networking Academy"
+          date: "2022",
+          title: "CCNA (Cisco Certified Network Associate)",
+          institution: "Cisco Networking Academy",
         },
         {
-            "date": "2018 - 2019",
-            "title": "Baccalaureate  Mathematical Sciences",
-            "institution": "Moulay Mohammed Ben Abdellah High School, Larache"
-        }
-    ],
-    
+          date: "2018 - 2019",
+          title: "Baccalaureate Mathematical Sciences",
+          institution: "Moulay Mohammed Ben Abdellah High School, Larache",
+        },
+      ],
     },
     experience: {
       p: "What I have done so far",
@@ -110,11 +120,8 @@ export const config: TConfig = {
     works: {
       p: "My work",
       h2: "Projects.",
-      content: `Following projects showcases my skills and experience through
-    real-world examples of my work. Each project is briefly described with
-    links to code repositories and live demos in it. It reflects my
-    ability to solve complex problems, work with different technologies,
-    and manage projects effectively.`,
+      content: `Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.`,
+      items: [], // Specify 'items' as an empty array or with actual content
     },
   },
 };
